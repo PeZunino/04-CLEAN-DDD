b25d8d0e44f2504cd5dfa4bd4b115bd2f32babc1
PeZunino <pedrozsantos@hotmail.com>
2025-02-26 16:30:28 -0300

    feat(entities): implement getters and setters

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Answer } from "../entities/answer"
 import { AnswersRepository } from "../repositories/answers-repository"
 
@@ -14,7 +15,11 @@ export class AnswerQuestionUseCase{
   ){}
 
   async execute({instructorId,questionId,content}:AnswerQuestionUseCaseRequest){
-    const answer = new Answer({authorId: instructorId,questionId,content})
+    const answer = Answer.create({
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+      content
+    })
 
     await this.answersRepository.create(answer)
     
