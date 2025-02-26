cb7f6814ea016fc958e081c378c8bbb6cf6570e7
PeZunino <pedrozsantos@hotmail.com>
2025-02-26 17:23:58 -0300

    chore(lint): configure ESLint integration

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,21 +1,22 @@
-import { AnswerQuestionUseCase } from './answer-question'
-import { AnswersRepository } from '../repositories/answers-repository'
-import { Answer } from '../entities/answer'
+import { Answer } from '../entities/answer';
+import { AnswersRepository } from '../repositories/answers-repository';
+import { AnswerQuestionUseCase } from './answer-question';
 
 const fakeAnswersRepository: AnswersRepository = {
-  async create(answer:Answer) {
-    return 
-  },
-}
+	async create(answer:Answer) {
+		return; 
+	},
+};
 
-test("create an answer", async ()=>{
-  const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository)
+test('create an answer', async ()=>{
+	const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository);
 
-  const answer = await answerQuestionUseCase.execute({
-    content: 'Nova resposta',
-    instructorId: '1',
-    questionId: '1'
-  })
+	const answer = await answerQuestionUseCase.execute({
+		content: 'Nova resposta',
+		instructorId: '1',
+		questionId: '1'
+	});
 
-  expect(answer.content).toEqual('Nova resposta')
-})
+	expect(answer.content)
+		.toEqual('Nova resposta');
+});
