8365108dc647d2e233429c8c073bf9920bc298c0
PeZunino <pedrozsantos@hotmail.com>
2025-02-27 18:34:48 -0300

    feat(question): get question by slug

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -2,9 +2,16 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
 import { Question } from '@/domain/forum/enterprise/entities/question';
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+
 	public items: Question[] = [];
 
 	async create(question: Question) {
 		this.items.push(question);
+	}
+
+	async findBySlug(slug: string): Promise<Question | null> {
+		const question = this.items.find(item=>item.slug.value == slug);
+		
+		return question ?? null;
 	}
 }
