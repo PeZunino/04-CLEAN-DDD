0d767ee91f3554e79910a1230b679b3287a4318b
PeZunino <pedrozsantos@hotmail.com>
2025-02-27 18:44:33 -0300

    feat(answer): delete answer

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -2,4 +2,8 @@ import { Answer } from '../../enterprise/entities/answer';
 
 export interface AnswersRepository{
 	create(answer:Answer):Promise<void>
+
+	findById(id: string): Promise<Answer | null>
+
+	delete(answer: Answer): Promise<void>
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,6 +4,18 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer';
 export class InMemoryAnswersRepository implements AnswersRepository {
 	public items: Answer[] = [];
 
+	async findById(id: string) {
+		const answer = this.items.find((item) => item.id.toString() === id);
+
+		return answer ?? null;
+	}
+
+	async delete(answer: Answer) {
+		const itemIndex = this.items.findIndex((item) => item.id === answer.id);
+
+		this.items.splice(itemIndex, 1);
+	}
+	
 	async create(answer: Answer) {
 		this.items.push(answer);
 	}
