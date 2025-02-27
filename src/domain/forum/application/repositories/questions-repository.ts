102d0bb62491da91d2391a4ccb982acd2119cfd1
PeZunino <pedrozsantos@hotmail.com>
2025-02-27 18:53:01 -0300

    feat(question): edit question

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,6 +1,8 @@
 import { Question } from '../../enterprise/entities/question';
 
 export interface QuestionsRepository{
+	save(question: Question): Promise<void>
+
 	create(question:Question):Promise<void>
 
 	findBySlug(slug:string):Promise<Question | null>
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -5,6 +5,12 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
 	public items: Question[] = [];
 
+	async save(question: Question) {
+		const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+		this.items[itemIndex] = question;
+	}
+	
 	async create(question: Question) {
 		this.items.push(question);
 	}
