b09b5e295201ffe0de24aedbc6e1cf9449df2031
PeZunino <pedrozsantos@hotmail.com>
2025-03-05 17:01:29 -0300

    feat(notification): read notification

--- src/domain/notification/application/repositories/notifications-repository.ts
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -2,4 +2,8 @@ import { Notification } from '../../enterprise/entities/notification';
 
 export interface NotificationsRepository {
 	create(notification: Notification): Promise<void>
+
+	findById(id: string): Promise<Notification | null>
+
+	save(notification: Notification): Promise<void>
 }
