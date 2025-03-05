6e777d229f65c3272f3910ed98a281be99c70c56
PeZunino <pedrozsantos@hotmail.com>
2025-03-05 17:01:09 -0300

    refactor: move application errors to core directory

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -2,8 +2,8 @@ import { Either, left, right } from '@/core/either';
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { AnswersRepository } from '../repositories/answers-repository';
-import { NotAllowedError } from './errors/not-allowed-error';
-import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { NotAllowedError } from '../../../../core/errors/errors/not-allowed-error';
+import { ResourceNotFoundError } from '../../../../core/errors/errors/resource-not-found-error';
 
 interface ChooseQuestionBestAnswerUseCaseRequest {
 	authorId: string
