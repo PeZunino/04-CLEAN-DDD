9a83181780d002c14502daa4bc271ec30645e350
PeZunino <pedrozsantos@hotmail.com>
2025-03-06 11:15:58 -0300

    feat(answer): sending notification

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,6 +1,7 @@
-import { Entity } from '@/core/entities/entity';
+import { AggregateRoot } from '@/core/entities/aggregate-root';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Optional } from '@/core/types/optional';
+import { AnswerCreatedEvent } from '../events/answer-created-event';
 import { AnswerAttachmentList } from './answer-attachment-list';
 
 export interface AnswerProps{
@@ -12,7 +13,7 @@ export interface AnswerProps{
 	updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
 	get content(){
 		return this.props.content;
 	}
@@ -60,12 +61,18 @@ export class Answer extends Entity<AnswerProps> {
 	}
 
 	static create(props:Optional<AnswerProps, 'createdAt' | 'attachments'>, id?:UniqueEntityID){
-		const question = new Answer({
+		const answer = new Answer({
 			...props,
 			attachments: props.attachments ?? new AnswerAttachmentList(),
 			createdAt: props.createdAt ?? new Date(), 
 		}, id);
  
-		return question;
+		const isNewAnswer = !id;
+
+		if (isNewAnswer) {
+			answer.addDomainEvent(new AnswerCreatedEvent(answer));
+		}
+
+		return answer;	
 	}
 }
